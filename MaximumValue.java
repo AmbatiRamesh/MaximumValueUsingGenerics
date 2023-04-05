a146ce28c39d6d360832d98000ffc0168744dfe4
“AmbatiRamesh” <rameshkohli401@gmail.com>
2023-04-05 15:14:45 +0530

    Added: all the 3 to one generic method

--- MaximumValue.java
+++ MaximumValue.java
@@ -1,8 +1,8 @@
 package com.bridgelabz.generics;
 
 public class MaximumValue {
-    public static <String extends Comparable<String>>  String maximum(String  A, String B, String C) {
-        String maximumNumber = A;
+    public static  <R extends Comparable<R>>  R maximum(R  A, R B, R C) {
+        R maximumNumber = A;
         if (A.compareTo(B) > 0 && A.compareTo(C) > 0)
             maximumNumber = A;
         else if (B.compareTo(A) > 0 && B.compareTo(C) > 0)
@@ -12,6 +12,8 @@ public class MaximumValue {
         return maximumNumber;
     }
     public static void main(String[] args) {
-        System.out.println("The max Number Among Three String numbers is: " + maximum("Virat","Dhoni","Rohit"));
+        System.out.println(maximum(20,40,30));
+        System.out.println(maximum(20.40F,40.20F,30.30F));
+        System.out.println(maximum("Virat","Dhoni","Rohit"));
     }
 }
